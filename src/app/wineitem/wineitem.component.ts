3fc37a465d2b12cf5c238603f5abd4bf3c9f2ae1
fcesc-code <bpfran@gmail.com>
2020-11-28 09:30:44 +0100

    first commit

---
+++ src/app/wineitem/wineitem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-wineitem',
+  templateUrl: './wineitem.component.html',
+  styleUrls: ['./wineitem.component.sass']
+})
+export class WineitemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
